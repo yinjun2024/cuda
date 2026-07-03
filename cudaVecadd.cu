5817a73a1ca4c9ef6e63e81d1eeae22e72a7bbdf
yinjun2024 <2829853731@qq.com>
2026-07-03 23:47:34 +0800

    add cuda memory management

--- cudaVecadd.cu
+++ cudaVecadd.cu
@@ -2,7 +2,7 @@
 #include <cuda_runtime.h>
 using namespace std;
 
-__global__ void vecAdd(float *A, float *B, float *C) {
+__global__ void vecAdd(float *A, float *B, float *C, int N) {
 	/*
 		threadIdx
 		blockDim
@@ -14,27 +14,31 @@ __global__ void vecAdd(float *A, float *B, float *C) {
 	// get the working index
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
-	C[idx] = A[idx] + B[idx];
+	if (idx < N) C[idx] = A[idx] + B[idx];
 }
 int main() {
 	const int N = 1024;
 	float *A, *B, *C;
-	A = new float[N];
-	B = new float[N];
-	C = new float[N];
+	cudaMallocManaged(&A, N * sizeof(float));
+	cudaMallocManaged(&B, N * sizeof(float));
+	cudaMallocManaged(&C, N * sizeof(float));
 	
 	mt19937 rnd(123);
 	auto distr = uniform_real_distribution<float>();
 	for (int i = 0; i < N; i++) A[i] = distr(rnd);
 	for (int i = 0; i < N; i++) B[i] = distr(rnd);
 
-	vecAdd<<<4, 256>>>(A, B, C);
+	const int threads = 256;
+	const int blocks = cuda::ceil_div(N, threads);
+	vecAdd<<<blocks, threads>>>(A, B, C, N);
 	// usage for >1 dim : MatAdd<<<dim3(16, 16), dim3(8, 8)>>>
+
+	cudaDeviceSynchronize();
 
 	for (int i = 0; i < N; i++) printf("%f%c", C[i], " \n"[i + 1 == N]);
 
-	delete[] A;
-	delete[] B;
-	delete[] C;
+	cudaFree(A);
+	cudaFree(B);
+	cudaFree(C);
 	return 0;
 }
