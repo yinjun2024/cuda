aedd46d42016a5df28cbd33e6e4019a484da6d72
yinjun2024 <2829853731@qq.com>
2026-07-05 15:00:29 +0800

    add vecmax py

---
+++ cudaVecmax.py
@@ -0,0 +1,25 @@
+import torch
+import time
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"device : {device}")
+
+size = 1 << 27
+a = torch.randn(size, device=device)
+
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
+
+for _ in range(3):
+    torch.reduce_max(a)
+    torch.cuda.synchronize() if device.type == "cuda" else None
+
+start_event.record()
+c = torch.reduce_max(a)
+end_event.record()
+
+torch.cuda.synchronize() if device.type == "cuda" else None
+
+print(f"result : \n{c}")
+
+print(f"timeused : {start_event.elapsed_time(end_event):.6f} ms")  # 结果会接近 1.54ms
