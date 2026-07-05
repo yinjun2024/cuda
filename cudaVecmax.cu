2c05ca8b184f25b3b0ce1420685e54f5d0fd75f1
yinjun2024 <2829853731@qq.com>
2026-07-05 15:15:52 +0800

    vecmax optimized

--- cudaVecmax.cu
+++ cudaVecmax.cu
@@ -5,25 +5,22 @@ using namespace std;
 
 template<int blockSize>
 __global__ void maxReduce(float *a, float *b, int n) {
-	__shared__ float s[blockSize];
+	__shared__ float s[blockSize]; 
 
-	int idx = threadIdx.x + 2 * blockSize * blockIdx.x;
+	int idx = threadIdx.x + blockSize * blockIdx.x;
 	int pos = threadIdx.x;
 
-	if (idx + blockSize < n) s[pos] = max(a[idx], a[idx + blockSize]);
-	else if (idx < n) s[pos] = a[idx];
+	float val = -FLT_MAX;
+	for (int i = idx; i < n; i += blockSize * gridDim.x) {
+		val = max(val, a[i]);
+	} s[pos] = val; __syncthreads();
 
-	int npos = n >= 2 * blockSize * blockIdx.x + blockSize ? blockSize : n - 2 * blockSize * blockIdx.x;
-
-	#define F(x) if (blockSize >= 2 * x) {if (pos < x && pos + x < npos) s[pos] = max(s[pos], s[pos + x]); __syncthreads();}
+	#define F(x) if (blockSize >= 2 * x) {if (pos < x) s[pos] = max(s[pos], s[pos + x]); __syncthreads();}
 	F(512) F(256) F(128) F(64)
 	#undef F
 
-	#define G(x) if (pos + x < npos) s[pos] = max(s[pos], s[pos + x]);
-	if (pos < 32) {
-		if (blockSize >= 2 * 32) G(32)
-		G(16) G(8); G(4) G(2) G(1)
-	}
+	#define G(x) if (blockSize >= 2 * x && pos < x) s[pos] = max(s[pos], s[pos + x]);
+	G(32) G(16) G(8); G(4) G(2) G(1)
 	#undef G
 
 	if (pos == 0) b[blockIdx.x] = s[0];
