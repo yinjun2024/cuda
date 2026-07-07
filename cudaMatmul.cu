58b8e0f90ef54578caa641145a14259bac4cf4f1
yinjun2024 <2829853731@qq.com>
2026-07-07 23:56:27 +0800

    bugfix

--- cudaMatmul.cu
+++ cudaMatmul.cu
@@ -17,16 +17,16 @@ __global__ void Matmul(float *A, float *B, float *C, int N, int M, int K) {
 	// ensure : threads(BS * BS), blocks(ceil(N / BN), ceil(M / BM))
 	// recommend : <128, 128, 8, 16>
 
+	constexpr int BAy = BK, BAx = BS * BS / BAy;
 	static_assert(BS * BS % BAy == 0);
+	constexpr int BBx = BK, BBy = BS * BS / BBx;
 	static_assert(BS * BS % BBx == 0);
+	constexpr int BCx = BN / BS, BCy = BM / BS;
 	static_assert(BN % BS == 0);
 	static_assert(BM % BS == 0);
-	constexpr int BAy = BK, BAx = BS * BS / BAy;
-	constexpr int BBx = BK, BBy = BS * BS / BBx;
-	constexpr int BCx = BN / BS, BCy = BM / BS;
+	constexpr int CA = BN / BAx, CB = BM / BBy;
 	static_assert(BN % BAx == 0);
 	static_assert(BM % BBy == 0);
-	constexpr int CA = BN / BAx, CB = BM / BBy;
 
 	int Ax = threadIdx.x / BAy, Ay = threadIdx.x % BAy;
 	int Bx = threadIdx.x / BBy, By = threadIdx.x % BBy;
