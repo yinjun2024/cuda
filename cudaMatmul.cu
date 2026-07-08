7f576b2a27ab13e1f74aabdb09a198b367875ead
yinjun2024 <2829853731@qq.com>
2026-07-08 22:56:27 +0800

    optimized

--- cudaMatmul.cu
+++ cudaMatmul.cu
@@ -12,30 +12,30 @@ using namespace std;
     } \
 } while(0)
 
-template<int BN, int BM, int BK, int BS>
+template<int BN, int BM, int BK, int BX, int BY, int WX, int WY>
 __global__ void Matmul(float *A, float *B, float *C, int N, int M, int K) {
 	// ensure : threads(BS * BS), blocks(ceil(N / BN), ceil(M / BM))
-	// recommend : <128, 128, 8, 16>
+	// recommend : <128, 128, 8, 8, 4, 8>
+	// BA : (8, 128), BB : (128, 8), BC : (8, 8)
 
-	constexpr int BAy = BK, BAx = BS * BS / BAy;
-	static_assert(BS * BS % BAy == 0);
-	constexpr int BBx = BK, BBy = BS * BS / BBx;
-	static_assert(BS * BS % BBx == 0);
-	constexpr int BCx = BN / BS, BCy = BM / BS;
-	static_assert(BN % BS == 0);
-	static_assert(BM % BS == 0);
-	constexpr int CA = BN / BAx, CB = BM / BBy;
+	static_assert(BN % BX == 0 && BM % BY == 0);
+	constexpr int BS = (BN / BX) * (BM / BY);
+	static_assert(BS % BK == 0);
+	constexpr int BAy = BK, BAx = BS / BAy;
+	constexpr int BBx = BK, BBy = BS / BBx;
 	static_assert(BN % BAx == 0);
 	static_assert(BM % BBy == 0);
+	constexpr int CA = BN / BAx, CB = BM / BBy;
 
 	int Ax = threadIdx.x / BAy, Ay = threadIdx.x % BAy;
 	int Bx = threadIdx.x / BBy, By = threadIdx.x % BBy;
-	int Cx = threadIdx.x / BS, Cy = threadIdx.x % BS;
-	Cx *= BCx; Cy *= BCy;
+	int Wx = threadIdx.x / (WX * WY), Wy = threadIdx.x % (WX * WY);
+	int Cx = Wx * BX + Wy / BY, Cy = Wx * BY + Wy % BY;
+	Cx *= BX; Cy *= BY;
 	int Sx = blockIdx.x * BN, Sy = blockIdx.y * BM;
 	
 	__shared__ float As[BN][BK + 1], Bs[BK + 1][BM]; // padding trick
-	float Areg[BCx], Breg[BCy], Creg[BCx][BCy] = {0};
+	float Areg[BX], Breg[BY], Creg[BX][BY] = {0};
 	
 	for (int k = 0; k < K; k += BK) {
 		#pragma unroll
@@ -52,22 +52,22 @@ __global__ void Matmul(float *A, float *B, float *C, int N, int M, int K) {
 
 		#pragma unroll
 		for (int x = 0; x < BK; x++) {
-			for (int _ = 0; _ < BCx; _++) Areg[_] = As[_ + Cx][x];
-			for (int _ = 0; _ < BCy; _++) Breg[_] = Bs[x][_ + Cy];
-			for (int i = 0; i < BCx; i++) for (int j = 0; j < BCy; j++) {
+			for (int _ = 0; _ < BX; _++) Areg[_] = As[_ + Cx][x];
+			for (int _ = 0; _ < BY; _++) Breg[_] = Bs[x][_ + Cy];
+			for (int i = 0; i < BX; i++) for (int j = 0; j < BY; j++) {
 				Creg[i][j] += Areg[i] * Breg[j];
 			}
 		}
 		__syncthreads();
 	}
 
-	for (int i = 0; i < BCx; i++) for (int j = 0; j < BCy; j++) {
+	for (int i = 0; i < BX; i++) for (int j = 0; j < BY; j++) {
 		int x = i + Cx + Sx, y = j + Cy + Sy;
 		if (x < N && y < M) C[x * M + y] = Creg[i][j];
 	}
 }
 
-template<int BN, int BM, int BK, int BS>
+template<int BN, int BM, int BK, int BX, int BY, int WX, int WY>
 void Matmul(int N, int M, int K) {
 	float *A, *B, *C;
 	float *devA, *devB, *devC;
@@ -90,12 +90,12 @@ void Matmul(int N, int M, int K) {
 	dim3 blocks(cuda::ceil_div(N, BN), cuda::ceil_div(M, BM));
 
 	for (int _ = 0; _ < 15; _++) {
-		Matmul<BN, BM, BK, BS><<<blocks, BS * BS>>>(devA, devB, devC, N, M, K);
+		Matmul<BN, BM, BK, BX, BY, WX, WY><<<blocks, (BN / BX) * (BM / BY)>>>(devA, devB, devC, N, M, K);
 		CUDA_CHECK(cudaDeviceSynchronize());
 	}
 
 	auto start = chrono::high_resolution_clock::now();
-	Matmul<BN, BM, BK, BS><<<blocks, BS * BS>>>(devA, devB, devC, N, M, K);
+	Matmul<BN, BM, BK, BX, BY, WX, WY><<<blocks, (BN / BX) * (BM / BY)>>>(devA, devB, devC, N, M, K);
 	CUDA_CHECK(cudaDeviceSynchronize());
 	auto end = chrono::high_resolution_clock::now();
 	chrono::duration<double, milli> dur = end - start;
@@ -126,7 +126,7 @@ void Matmul(int N, int M, int K) {
 
 int main() {
 	const int S = 1 << 13;
-	Matmul<128, 128, 8, 16>(S, S, S);
+	Matmul<128, 128, 8, 8, 4, 8>(S, S, S);
 	// Matmul<64, 64, 16, 16>(1 << 13, 1 << 13, 1 << 13);
 	// Matmul<64, 64, 16, 32>(1 << 13, 1 << 13, 1 << 13);
 }
