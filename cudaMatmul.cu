9d6e1173361048446525d1761408572ba0a669ad
yinjun2024 <2829853731@qq.com>
2026-07-10 22:49:52 +0800

    add async implement

--- cudaMatmul.cu
+++ cudaMatmul.cu
@@ -12,7 +12,7 @@ using namespace std;
     } \
 } while(0)
 
-__global__ void Matmul(float *A, float *B, float *C, int N, int M, int K) {
+__global__ void Matmul_sync(float *A, float *B, float *C, int N, int M, int K) {
     // blocks(ceil_div(N, 128), ceil_div(M, 128)), threads(256)
     // restriction : N, M, K must aligned with 128 !!!
 
@@ -33,6 +33,63 @@ __global__ void Matmul(float *A, float *B, float *C, int N, int M, int K) {
         x = Bpos | Bx << 2, y = k | By;
         reinterpret_cast<float4*>(Bs[By] + (Bx << 2))[0] = reinterpret_cast<float4*>(B + y * M + x)[0];
         __syncthreads();
+
+        #pragma unroll
+        for (int k_ = 0; k_ < 8; k_++) {
+            reinterpret_cast<float4*>(Areg)[0] = reinterpret_cast<float4*>(AsT[k_] + (Cx << 2))[0];
+            reinterpret_cast<float4*>(Areg)[1] = reinterpret_cast<float4*>(AsT[k_] + (Cx << 2 | 64))[0];
+            reinterpret_cast<float4*>(Breg)[0] = reinterpret_cast<float4*>(Bs[k_] + (Cy << 2))[0];
+            reinterpret_cast<float4*>(Breg)[1] = reinterpret_cast<float4*>(Bs[k_] + (Cy << 2 | 64))[0];
+            #pragma unroll
+            for (int i = 0; i < 8; i++) {
+                #pragma unroll
+                for (int j = 0; j < 8; j++) {
+                    Creg[i][j] += Areg[i] * Breg[j];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) {
+        int x = Apos | i >> 2 << 6 | Cx << 2 | (i & 3), y = Bpos | j >> 2 << 6 | Cy << 2 | (j & 3);
+        C[x * M + y] = Creg[i][j];
+    }
+}
+
+__global__ void Matmul_async(float *A, float *B, float *C, int N, int M, int K) {
+    // blocks(ceil_div(N, 128), ceil_div(M, 128)), threads(256)
+    // restriction : N, M, K must aligned with 128 !!!
+
+    __shared__ float AsT[8][128], Bs[8][128];
+    float Areg[8], Breg[8], Creg[8][8] = {0}; float4 valA[2], valB[2]; int x, y;
+    int Apos = blockIdx.y << 7, Ax = threadIdx.x >> 7, Ay = threadIdx.x & 127;
+    int Bpos = blockIdx.x << 7, Bx = threadIdx.x & 31, By = threadIdx.x >> 5;
+    int Warp = threadIdx.x >> 5, Lane = threadIdx.x & 31;
+    int Cx = (Warp & 1) << 3 | Lane >> 3 << 1 | Lane & 1;
+    int Cy = Warp >> 1 << 2 | (Lane >> 1 & 3); // Z-Shape
+
+	int op = 0;
+	x = Ax << 2, y = Apos | Ay;
+	valA[!op] = reinterpret_cast<float4*>(A + y * K + x)[0];
+    x = Bpos | Bx << 2, y = By;
+	valB[!op] = reinterpret_cast<float4*>(B + y * M + x)[0];
+
+    for (int k = 0; k < K; k += 8) {
+		op ^= 1;
+
+		if (k + 8 < K) {
+			x = (k + 8) | Ax << 2, y = Apos | Ay;
+			valA[!op] = reinterpret_cast<float4*>(A + y * K + x)[0];
+			x = Bpos | Bx << 2, y = (k + 8) | By;
+			valB[!op] = reinterpret_cast<float4*>(B + y * M + x)[0];
+		}
+
+		AsT[Ax << 2 | 0][Ay] = valA[op].x;
+		AsT[Ax << 2 | 1][Ay] = valA[op].y;
+		AsT[Ax << 2 | 2][Ay] = valA[op].z;
+		AsT[Ax << 2 | 3][Ay] = valA[op].w;
+		reinterpret_cast<float4*>(Bs[By] + (Bx << 2))[0] = valB[op];
 
         #pragma unroll
         for (int k_ = 0; k_ < 8; k_++) {
@@ -79,7 +136,7 @@ void Matmul(int N, int M, int K) {
 	dim3 blocks(cuda::ceil_div(N, 128), cuda::ceil_div(M, 128));
 
 	for (int _ = 0; _ < 15; _++) {
-		Matmul<<<blocks, 256>>>(devA, devB, devC, N, M, K);
+		Matmul_async<<<blocks, 256>>>(devA, devB, devC, N, M, K);
 		CUDA_CHECK(cudaDeviceSynchronize());
 	}
 	
@@ -89,7 +146,7 @@ void Matmul(int N, int M, int K) {
     cudaEventCreate(&stop);
 	cudaEventRecord(start, 0);
 
-	Matmul<<<blocks, 256>>>(devA, devB, devC, N, M, K);
+	Matmul_async<<<blocks, 256>>>(devA, devB, devC, N, M, K);
 	
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
